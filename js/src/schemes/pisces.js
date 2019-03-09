d8231e0858720480b1917fb543f77040c8ea9f19
logTXT <logtxt@qq.com>
2019-03-09 16:48:14 +0800

    Site updated: 2019-03-09 16:48:13

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,1 +1,3 @@
+// build time:Sat Mar 09 2019 16:46:38 GMT+0800 (中国标准时间)
 $(document).ready(function(){var e=$(".sidebar-inner"),t=CONFIG.sidebar.offset||12;function i(){var i,o,a,f=$(".header-inner").height()+t,n=(i=$("#footer"),o=$(".footer-inner"),a=i.outerHeight()-o.outerHeight(),i.outerHeight()+a);f+($("#sidebar").height()+NexT.utils.getSidebarb2tHeight())<$("#content").height()&&(e.affix({offset:{top:f-t,bottom:n}}),e.affix("checkPosition")),$("#sidebar").css({"margin-top":f,"margin-left":"auto"})}i(),window.matchMedia("(min-width: 992px)").addListener(function(t){t.matches&&($(window).off(".affix"),e.removeData("bs.affix").removeClass("affix affix-top affix-bottom"),i())})});
+//rebuild by neat 
--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,1 +1,3 @@
+// build time:Sat Mar 09 2019 16:46:38 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o,i=window.location.href.replace(window.location.origin,"");if($(window).on("scroll",function(){clearTimeout(o),o=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+i,{expires:365,path:""})},250)}),void 0!==Cookies.get("scroll-cookie")){var e=Cookies.get("scroll-cookie").split("|");e[1]===i&&$(window).scrollTop(e[0])}});
+//rebuild by neat 
