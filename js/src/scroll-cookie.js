03b30209ee2b76121d3c10bb7935bf30f1da67ae
logTXT <logtxt@qq.com>
2019-03-10 03:52:22 +0800

    Site updated: 2019-03-10 03:52:21

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,1 @@
-// build time:Sat Mar 09 2019 21:45:45 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o,i=window.location.href.replace(window.location.origin,"");if($(window).on("scroll",function(){clearTimeout(o),o=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+i,{expires:365,path:""})},250)}),void 0!==Cookies.get("scroll-cookie")){var e=Cookies.get("scroll-cookie").split("|");e[1]===i&&$(window).scrollTop(e[0])}});
-//rebuild by neat 
